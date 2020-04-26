74d9bb9aabbe28c2b1bc3fcf91b5d88582afa485
Will Traweek <will.traweek@gmail.com>
2020-04-26 15:09:27 -0500

    create default box implementation

---
+++ box.py
@@ -0,0 +1,7 @@
+class Box:
+    """The items that we have to ship"""
+
+    def __init__(self, weight, name, value):
+        self.weight = weight
+        self.name = name
+        self.value = value
