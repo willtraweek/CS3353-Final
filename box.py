c559bcc22ca6a907f68e39ce7d7f2f9e8d576452
Will Traweek <will.traweek@gmail.com>
2020-04-26 15:20:25 -0500

    reformat initializer for box

--- box.py
+++ box.py
@@ -1,7 +1,8 @@
 class Box:
     """The items that we have to ship"""
 
-    def __init__(self, weight, name, value):
-        self.weight = weight
+    def __init__(self, name, weight, value):
+        """Creates the box"""
         self.name = name
+        self.weight = weight
         self.value = value
