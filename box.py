2c8ad081c964be24277063fce2feb5e5be146b6e
Will Traweek <will.traweek@gmail.com>
2020-04-26 15:20:42 -0500

    create __str__ for box

--- box.py
+++ box.py
@@ -6,3 +6,6 @@ class Box:
         self.name = name
         self.weight = weight
         self.value = value
+
+    def __str__(self):
+        return(f"{self.name}: {self.weight}lbs | ${self.value}")
