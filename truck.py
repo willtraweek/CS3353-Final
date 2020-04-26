ff02b80249c15ae4469a14808f08f1ba409f2f58
Will Traweek <will.traweek@gmail.com>
2020-04-26 16:26:30 -0500

    add basic truck initialization

--- truck.py
+++ truck.py
@@ -1,7 +1,17 @@
 class Truck:
-    """Truck that we'll be filling with Boxes"""
+    """Truck that we'll be filling with Boxes
+
+    Attributes:
+        capacity: max weight that the truck can hold
+        weight: current weight that the truck holds
+        value: the price of the boxes currently in the truck
+        boxes: dictionary of all boxes where the key is the box and the value is the quantity
+    """
 
     def __init__(self, capacity):
         self.capacity = capacity
+        self.weight = 0
+        self.value = 0
+        self.boxes = {} #empty dictionary to start
 
     def add_box(self, Box):
