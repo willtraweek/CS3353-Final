9d168c483957fd7b15fd1616158356561e4f7b8b
Will Traweek <will.traweek@gmail.com>
2020-04-26 17:14:25 -0500

    add BoxAdditionError

--- exceptions.py
+++ exceptions.py
@@ -13,3 +13,16 @@ class BoxRemovalError(Error):
     def __init__(self, message, box):
         print(message)
         print(box)
+
+class BoxAdditionError(Error):
+    """Error that occurs when a truck cannot add a box
+
+    Attributes:
+        message: explanation of the error
+        capacity: capacity
+        box: copy of the box that couldn't be added
+    """
+
+    def __init__(self, message, weight, capacity, box):
+        print(message)
+        print(f"Weight: {weight} Capacity: {capacity}\n{box}")
