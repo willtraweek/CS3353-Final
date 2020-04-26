eb31f3935b7ce7505613002f23c1400217ae12a2
Will Traweek <will.traweek@gmail.com>
2020-04-26 16:39:07 -0500

    add exceptions.BoxRemovalError

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,12 @@
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
+
+class BoxRemovalError(Error):
+    """Error that occurs when a truck cannot remove a box
+
+    Attributes:
+        message: explanation of the error
+    """
+    def __init__(self, message):
+        self.message = message
