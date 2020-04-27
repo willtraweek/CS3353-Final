ba6643d46c676083665cde372dfbbb6363e00142
Will Traweek <will.traweek@gmail.com>
2020-04-27 04:25:19 -0500

    stage main to test out warehouse on github

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from box import Box
 from truck import Truck
+from warehouse import Warehouse
 from utilities import set_creation
 
 
 def main():
     # for i in range(10):
     #     set_creation.create_test_set(numBoxes=i*10, maxWeight=i*10, maxPrice=i*15, test_num=i+1)
-    print("main run successfully")
+    warehouse = Warehouse("./examples/available_boxes.csv")
 
 
 if __name__ == "__main__":
