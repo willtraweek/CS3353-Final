89391d196e77eca0abc1d3b4f61ca19a99a2a869
Will Traweek <will.traweek@gmail.com>
2020-04-27 00:40:32 -0500

    formatting for PEP-8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utilities import set_creation
 
 def main():
     # for i in range(10):
-    # set_creation.create_test_set(numBoxes=i*10, maxWeight=i*10, maxPrice=i*15, test_num=i+1)
+    #     set_creation.create_test_set(numBoxes=i*10, maxWeight=i*10, maxPrice=i*15, test_num=i+1)
     print("main run successfully")
 
 
