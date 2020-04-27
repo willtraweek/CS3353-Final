a0f114bf7f8b3ace1731be46dd54dbe28295a102
Will Traweek <will.traweek@gmail.com>
2020-04-27 04:25:04 -0500

    add warehouse.import_boxes from file

--- warehouse.py
+++ warehouse.py
@@ -31,6 +31,19 @@ class Warehouse:
         self.value = 0
         self.import_boxes(input_file_path)
 
+    def import_boxes(self, input_file_path):
+        """reads in boxes from a file and adds them to the dictionary"""
+        input_file = open(input_file_path, "r")
+
+        for line in input_file.readlines():
+            name, weight, price = line.split(",")
+
+            #prevents the first line from being read in
+            if name == "name":
+                continue
+
+            self.add_box(Box(name, int(weight), int(price)))
+
     def add_truck(self, capacity):
         """Creates a new truck and adds it to the list"""
         self.trucks.add(Truck(capacity))
