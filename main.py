2a19766ab60146f6018cab6e2b2f4179fa26db06
Will Traweek <will.traweek@gmail.com>
2020-04-27 00:39:59 -0500

    add test set creation to main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from box import Box
 from truck import Truck
+from utilities import set_creation
+
 
 def main():
+    # for i in range(10):
+    # set_creation.create_test_set(numBoxes=i*10, maxWeight=i*10, maxPrice=i*15, test_num=i+1)
     print("main run successfully")
 
 
