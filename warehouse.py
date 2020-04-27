ed00fae018547e6ce4d5fec6f1eb8c28c5f7d311
Will Traweek <will.traweek@gmail.com>
2020-04-27 00:45:00 -0500

    add box handling (copied over from trucks)

--- warehouse.py
+++ warehouse.py
@@ -1,5 +1,7 @@
 #  Copyright (c) 2020, Will Traweek, All Rights Reserved.
 
+from exceptions import *
+
 class warehouse:
     """A warehouse stores boxes for shipping.  It can read in manifests and export them.  It also has a list of
     trucks assigned to it.  The largest part of the warehouse's job, though, is preparing said trucks for shipping.
@@ -17,3 +19,32 @@ class warehouse:
         self.boxes = {}
         self.capacity = capacity
         self.value = 0
+
+    def add_box(self, box):
+        """add the passed box to the warehouse"""
+        if self.weight + box.weight <= self.capacity:
+            if box in self.boxes:
+                self.boxes[box] += 1
+            else:
+                self.boxes[box] = 1
+
+            self.weight += box.weight
+            self.value += box.price
+        else:
+            # if adding this box would overfill the truck
+            raise BoxAdditionError("Box too heavy to fit", self.weight, self.capacity, box)
+
+    def remove_box(self, box):
+        """removes the passed box from the warehouse"""
+        if box not in self.boxes:
+            raise BoxRemovalError("Box not in warehouse", box)
+        elif self.boxes[box] == 0:
+            raise BoxRemovalError("No boxes of this type left in warehouse", box)
+
+        self.weight -= box.weight
+        self.value -= box.price
+        self.boxes[box] -= 1
+
+        # if the removed item was the final one of its type, remove it
+        if self.boxes[box] == 0:
+            self.boxes.pop(box)
