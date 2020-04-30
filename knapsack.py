11d062d9789a533e6933afaa8ba9dce4296fb710
Will Traweek <will.traweek@gmail.com>
2020-04-30 02:11:16 -0500

    add greedy algorithm implementation of knapsack algorithm

--- knapsack.py
+++ knapsack.py
@@ -6,6 +6,16 @@ from exceptions import *
 import sys
 
 
+def greedy_algorithm(warehouse, truck):
+    while len(warehouse.boxes) > 0 and truck.leftover_space() > 0:
+        try:
+            box = most_expensive_box(truck.leftover_space(), warehouse.boxes)
+            warehouse.load_box(box, truck)
+        except IncompatibleBoxError:
+            # if there are no compatible boxes left, then return
+            return
+
+
 def most_expensive_box(max_weight, boxes):
     # create a temporary box with maximum weight and minimum price to start
     output_box = Box("temp", sys.maxsize, 0)
