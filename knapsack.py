c4b6bb89da19d30a485909d5f73adc70d736e0ad
Will Traweek <will.traweek@gmail.com>
2020-04-30 02:23:05 -0500

    clean up and comment through the knapsack algorithms

--- knapsack.py
+++ knapsack.py
@@ -7,6 +7,13 @@ import sys
 
 
 def greedy_algorithm(warehouse, truck):
+    """This is the trivial implementation.  It finds the most expensive item that will still fit in the truck and loads
+    it into the truck.
+
+    Attributes:
+        warehouse: This is the location of all of the stored boxes that need to be loaded
+        truck: The truck that the algorithm is attempting to fill
+    """
     while len(warehouse.boxes) > 0 and truck.leftover_space() > 0:
         try:
             box = most_expensive_box(truck.leftover_space(), warehouse.boxes)
@@ -17,6 +24,12 @@ def greedy_algorithm(warehouse, truck):
 
 
 def most_expensive_box(max_weight, boxes):
+    """This finds the most expensive box that still falls under the maximum weight requirements given.
+
+    Attributes:
+        max_weight: The maximum amount of weight that the box can be
+        boxes: a dictionary containing many boxes to iterate through
+    """
     # create a temporary box with maximum weight and minimum price to start
     output_box = Box("temp", sys.maxsize, 0)
     for box in boxes:
