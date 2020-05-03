6b896544072f1d95548faa97b495a5c542a05e94
Will Traweek <will.traweek@gmail.com>
2020-05-03 01:34:35 -0500

    complete dynamic_programming_solution

--- knapsack.py
+++ knapsack.py
@@ -15,11 +15,55 @@ def dynamic_programming_solution(warehouse, truck):
         warehouse: This is the location of all of the stored boxes that need to be loaded
         truck: The truck that the algorithm is attempting to fill
     """
-    solution = []
+    # create 2d array to store values.  Each row represents an item and each column a capacity.  Each cell is the
+    # optimal solution for the items above it and the capacity that it currently represents.  By the time the bottom
+    # of the table has been reached, the optimal solutions for (0, capacity] will have been found.
 
-    for i in range(warehouse.num_boxes()):
-        for j in range(truck.capacity):
-            return
+    solution = np.zeros((warehouse.num_boxes(), truck.capacity), dtype=Truck)
+    boxes = warehouse.to_list()
+
+    for x in range(solution.shape[0]):
+        for y in range(solution.shape[1]):
+            # this is made slightly more complicated by the fact that numpy allows negative indexes.  Hence, the odd
+            # way of going about both indexing and the top two if statements.  However without these, there is no way
+            # to properly check for edge cases
+            if y < boxes[x].weight:
+                solution[x, y] = Truck(truck.capacity)
+                continue
+            if x == 0:
+                # this means there is no previous data on the row above, so we'll have to assume that the current item
+                # is the highest value for now
+                temp = Truck(truck.capacity)
+                temp.add_box(boxes[x])
+                solution[x, y] = temp
+                continue
+
+            prev_max = solution[x-1, y]
+
+            # make a copy of the best possible truck that we could add the current item to
+            temp_truck = solution[x-1, y-boxes[x].weight]
+            curr_max = Truck(truck.capacity)
+            for key in temp_truck.boxes.keys():
+                # the reason we can't simply copy the dictionary over, is that the other values (like weight and value)
+                # would not stay the same.  We could copy those over individually as well, but this helps future-proof
+                # at the cost of a small amount of run-time
+                curr_max.add_box(key)
+            # add the current item to that truck
+            curr_max.add_box(boxes[x])
+
+            if prev_max == None:
+                solution[x, y] = curr_max
+            elif prev_max.value < curr_max.value:
+                solution[x, y] = curr_max
+            else:
+                solution[x, y] = prev_max
+
+    for box, quantity in solution[-1, -1].boxes.items():
+        for i in range(quantity):
+            # this loads the found optimal solution into the given truck
+            warehouse.load_box(box, truck)
+
+
 
 
 def greedy_solution(warehouse, truck):
