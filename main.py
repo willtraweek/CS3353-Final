4301731a7b188ef3c53528fc2b3bb804f0b08c54
Will Traweek <will.traweek@gmail.com>
2020-05-06 22:15:48 -0500

    add evaluate_algorithms

--- main.py
+++ main.py
@@ -21,5 +21,28 @@ def main():
     evaluate_algorithms(algorithms_list, algorithm_names, 0, 10)
 
 
+def evaluate_algorithms(algorithm_list, algorithm_names, x_min, x_max):
+    """This sets up the collection and management of the rest of the data"""
+    x = range(x_min + 1, x_max + 1)
+
+    y_value = []
+    y_time = []
+
+    for j in range(len(algorithm_list)):
+        temp_value_list = []
+        temp_time_list = []
+        algorithm = algorithm_list[j]
+
+        for i in x:
+            value, temp_time = setup_graph(algorithm, i)
+            temp_value_list.append(value)
+            temp_time_list.append(temp_time)
+
+        y_value.append(temp_value_list)
+        y_time.append(temp_time_list)
+
+    graph_algorithms(x, algorithm_names, y_value, y_time, y_value="Value", y_time="Time")
+    save_to_csv(x, algorithm_names, y_value, y_time, y_value="Value", y_time="Time")
+
 if __name__ == "__main__":
     main()
