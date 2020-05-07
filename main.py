f736563f1d2daf23c159462713868ade03245d2a
Will Traweek <will.traweek@gmail.com>
2020-05-06 20:53:24 -0500

    add graphing request to main

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from truck import Truck
 from warehouse import Warehouse
 from utilities import set_creation
 import knapsack
+import time
+import matplotlib.pyplot as plt
+
 
 def main():
-    # for i in range(10):
-    #     set_creation.create_test_set(numBoxes=i*10, maxWeight=i*10, maxPrice=i*15, test_num=i+1)
-    warehouse = Warehouse("./examples/available_boxes_10.csv")
-    truck = Truck(1000)
-    warehouse.add_truck(truck)
-    knapsack.dynamic_programming_solution(warehouse, truck) # 2252
-    #knapsack.greedy_solution(warehouse, truck) # 1798, 2877
-    print(truck.value)
+    # for i in range(1000):
+    #    set_creation.create_test_set(numBoxes=(i+1)*10, maxWeight=100, maxPrice=500, test_num=i+1)
+
+    algorithms_list = [knapsack.dynamic_programming_solution,
+                       knapsack.greedy_solution]
+    algorithm_names = ["Dynamic Programming",
+                       "Greedy Solution"]
+    # it took me hours to do a full run on my computer, so I modified the code below to do a shortened version as a
+    # proof of concept on github.  If you want to run it with the same settings that I did, you can run it with
+    # 0 and 150 as the two numbers in the function below
+    evaluate_algorithms(algorithms_list, algorithm_names, 0, 150)
+
 
 if __name__ == "__main__":
     main()
